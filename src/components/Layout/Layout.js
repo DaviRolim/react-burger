4371066c3002f854ac08bb6cbdbcdc961058b518
davi.holanda <davi.holanda@mv.com.br>
2019-02-14 18:09:04 -0300

    responsive toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,26 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 import styles from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Fragment>
-        <Toolbar />
-        <SideDrawer />
-        <main className={styles.Content}>
-            {props.children}
-        </main>
-    </Fragment>
-)
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+    render () {
+        return (
+            <Fragment>
+            <Toolbar />
+            <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+            <main className={styles.Content}>
+                {this.props.children}
+            </main>
+            </Fragment>
+    )}
+    
+}
 
-export default layout
+export default Layout
